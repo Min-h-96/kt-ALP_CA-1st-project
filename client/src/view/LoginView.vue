7eeb2a525abbb1c56e80be3cbcd5be1a54c86e20
Min-h-96 <asdf6256@gmail.com>
2024-09-09 15:23:26 +0900

    feat: 로그인 상태에서 LoginView 요청 시, MainView 로 이동

--- client/src/view/LoginView.vue
+++ client/src/view/LoginView.vue
@@ -1,24 +1,42 @@
 <script setup>
 import axios from "axios";
 import { useRouter } from "vue-router";
-import { useTemplateRef } from "vue";
+import { onBeforeMount, useTemplateRef } from "vue";
+
+onBeforeMount(() => {
+  const checkLoginState = async () => {
+    await axios
+      .get("http://localhost:8080/api/session/user", {
+        header: {
+          "Context-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        router.replace("/");
+      })
+      .catch((err) => {});
+  };
+  checkLoginState();
+});
 
 const router = useRouter();
-const id = useTemplateRef('input-id');
-const password = useTemplateRef('input-password');
+const id = useTemplateRef("input-id");
+const password = useTemplateRef("input-password");
 
 const requestLogin = async () => {
   await axios
-    .post("http://localhost:8080/api/login",
+    .post(
+      "http://localhost:8080/api/login",
       {
         id: id.value.value,
-        password: password.value.value
+        password: password.value.value,
       },
       {
         header: {
           "Context-Type": "application/json",
         },
-      })
+      }
+    )
     .then((res) => {
       router.push("/");
     })
@@ -31,12 +49,9 @@ const requestLogin = async () => {
 <template>
   <div class="bg-gradient-primary h-100">
     <div class="container">
-
       <!-- Outer Row -->
       <div class="row justify-content-center">
-
         <div class="col-xl-10 col-lg-12 col-md-9">
-
           <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="card-body p-0">
               <!-- Nested Row within Card Body -->
@@ -45,28 +60,48 @@ const requestLogin = async () => {
                 <div class="col-lg-6">
                   <div class="p-5">
                     <div class="text-center">
-                      <h1 class="h4 text-gray-900 mb-4">Welcome KT AICC Manager!</h1>
+                      <h1 class="h4 text-gray-900 mb-4">
+                        Welcome KT AICC Manager!
+                      </h1>
                     </div>
                     <form class="user">
                       <div class="form-group">
-                        <input type="text" class="form-control form-control-user" ref="input-id"
-                          aria-describedby="emailHelp" placeholder="Enter ID">
+                        <input
+                          type="text"
+                          class="form-control form-control-user"
+                          ref="input-id"
+                          aria-describedby="emailHelp"
+                          placeholder="Enter ID"
+                        />
                       </div>
                       <div class="form-group">
-                        <input type="password" class="form-control form-control-user" ref="input-password"
-                          placeholder="Enter Password">
+                        <input
+                          type="password"
+                          class="form-control form-control-user"
+                          ref="input-password"
+                          placeholder="Enter Password"
+                        />
                       </div>
                       <div class="form-group">
                         <div class="custom-control custom-checkbox small">
-                          <input type="checkbox" class="custom-control-input" id="customCheck">
-                          <label class="custom-control-label" for="customCheck">Remember Me</label>
+                          <input
+                            type="checkbox"
+                            class="custom-control-input"
+                            id="customCheck"
+                          />
+                          <label class="custom-control-label" for="customCheck"
+                            >Remember Me</label
+                          >
                         </div>
                       </div>
-                      <div v-on:click="requestLogin" class="btn btn-primary btn-user btn-block">
+                      <div
+                        v-on:click="requestLogin"
+                        class="btn btn-primary btn-user btn-block"
+                      >
                         Login
                       </div>
                     </form>
-                    <hr>
+                    <hr />
                     <div class="text-center">
                       <a class="small" href="#">Forgot Password?</a>
                     </div>
@@ -78,9 +113,7 @@ const requestLogin = async () => {
               </div>
             </div>
           </div>
-
         </div>
-
       </div>
     </div>
   </div>
@@ -88,6 +121,6 @@ const requestLogin = async () => {
 
 <style scoped>
 .h-100 {
-  height: 100vh !important
+  height: 100vh !important;
 }
 </style>
